08011c557b05057110748255274beb1aef26c519
Shourya Rathor <103272518+Hamkill@users.noreply.github.com>
2023-02-03 21:33:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-const qrcode = require('qrcode-terminal');
+const http = require('http');
+const hostname = '0.0.0.0';
+const port = 3000; 
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const server = http.createServer((req, res) => {
 
-const client = new Client({
-    authStrategy: new LocalAuth()
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('it works!');
+	
+
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', message => {
-	if(message.body === 'ping') {
-		message.reply('pong');
-        
-
-	}else{
-        var msg = message.body;
-        message.sendMessage(msg);
-        console.log('Replied the recived message to the user ');
-    }
-});
-
-client.on('message', message => {
-	if(message.body === 'hi') {
-		message.reply('hey there');
-        console.log('Replied "hey there" to ');
-	}
-});
-
-client.initialize();
+server.listen(port, hostname, () => {console.log('running');});
